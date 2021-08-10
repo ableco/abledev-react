6df477193ce282b5362e8a9b8dac2866460bd4c6
Julio García <julio.garcia@hey.com>
2021-08-10 01:25:05 -0500

    expose useQueryClient

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useMutation } from "./useMutation";
 export { default as useQuery } from "./useQuery";
+export { default as useQueryClient } from "./useQueryClient";
 export { default as wrapRootComponent } from "./wrapRootComponent";
